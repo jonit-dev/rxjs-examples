0b21d2eb0d54e208f465ad4da7ba89a760b68d7f
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-06-22 15:18:28 -0700

    finish :

--- src/operators.js
+++ src/operators.js
@@ -17,3 +17,43 @@
 // const subscription = observable.subscribe(console.log);
 
 // DOM EVENT OPERATORS ========================================
+
+// import { fromEvent } from "rxjs";
+
+// const observable = fromEvent(document, "click");
+
+// const subscription = observable.subscribe(console.log);
+
+// from OPERATOR ========================================
+
+// import { from } from "rxjs";
+
+// const observable = from(fetch("https://jsonplaceholder.typicode.com/todos/1"));
+
+// const subscription = observable.subscribe({
+//   next(value) {
+//     console.log(value);
+//   },
+//   complete(value) {
+//     console.log("complete");
+//   },
+// });
+
+//* PIPEABLE OPERATORS ========================================
+// Functions for transforming, filtering and combining data.
+
+//* most popular one: map operator
+
+import { of } from "rxjs";
+import { map } from "rxjs/operators";
+
+const observable = of(1, 2, 3, 4, 5).pipe(map((value) => `$${value}`));
+
+const subscription = observable.subscribe({
+  next(value) {
+    console.log(value);
+  },
+  complete(value) {
+    console.log("complete");
+  },
+});
